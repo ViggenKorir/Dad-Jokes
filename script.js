50181996d2e7eb1e1a652ceb1d9b0ebeb0502a87
Viggen <koriremanuel0@gmail.com>
2024-07-19 16:14:47 +0300

    Update Dad-Jokes

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ document.getElementById('dadJokeBtn').addEventListener('click', function (event)
       document.getElementById('jokeDisplay').textContent = 'Failed to fetch a joke!';
     });
 });
+
+const form = document.getElementById('userForm');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent form submission
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let comment = document.getElementById('comment').value;
+
+  if (name === '' || email === '') {
+    alert('Please fill in the required fields (name and email).');
+    return;
+  }
+
+  let userData = {
+    name: name,
+    email: email,
+    comment: comment
+  };
+
+ 
+});
+
